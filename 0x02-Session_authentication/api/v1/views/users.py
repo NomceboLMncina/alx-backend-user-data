d36d470c38d0fe3eb720d55dd1ed00c5b8f1d14d
NomceboLMncina <nomcebolungapearlmncina@gmail.com>
2025-05-29 19:26:14 +0000

    changes

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -30,12 +30,10 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == "me":
         if request.current_user is None:
             abort(404)
-        user = request.current_user
-        return jsonify(user.to_json())
+        return jsonify(request.current_user.to_json())
+    
     user = User.get(user_id)
     if user is None:
-        abort(404)
-    if request.current_user is None:
         abort(404)
     return jsonify(user.to_json())
 
@@ -46,7 +44,7 @@ def delete_user(user_id: str = None) -> str:
     Path parameter:
       - User ID
     Return:
-      - empty JSON is the User has been correctly deleted
+      - empty JSON if the User has been correctly deleted
       - 404 if the User ID doesn't exist
     """
     if user_id is None:
